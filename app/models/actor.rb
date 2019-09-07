d72335aed8f43e737ea2fb19e08631676f832f0f
Oscar Bernardini <bernardini687@gmail.com>
2019-09-07 16:09:07 +0200

    move method full_name in models/concerns

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,6 @@
 class Actor < ApplicationRecord
   has_many :characters
   has_many :shows, through: :characters
+
+  include Nameable
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -5,7 +5,5 @@ class Character < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true, allow_nil: true
 
-  def full_name
-    [first_name, last_name].compact.join ' '
-  end
+  include Nameable
 end

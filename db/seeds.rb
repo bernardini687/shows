dd2b95993b95b64b56b4c59cf2802337100d7138
Oscar Bernardini <bernardini687@gmail.com>
2019-09-07 15:50:40 +0200

    code seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+<<~SRC
+  destroy all shows, actors, characters
+  create shows: Final Space; Parks and Recreation; The Office; Master of None
+  create actors: Aziz, Ansari; Rashida, Jones; Olan, Rogers
+  add characters:
+    Dev, *, Master of None, Aziz;
+    Tom, Haverford, Parks and Recreation, Aziz;
+    Ann, Perkins, Parks and Recreation, Rashida;
+    Karen, Filippelli, The Office, Rashida;
+    Gary, Goodspeed, Final Space, Olan
+  done
+SRC
+
+puts 'clearing shows'
+Show.destroy_all
+
+puts 'clearing actors'
+Actor.destroy_all
+
+puts 'clearing characters'
+Character.destroy_all
+
+puts 'generating shows'
+final_space    = Show.create! name: 'Final Space'
+parks_and_rec  = Show.create! name: 'Parks and Recreation'
+the_office     = Show.create! name: 'The Office'
+master_of_none = Show.create! name: 'Master of None'
+
+puts 'generating actors'
+aziz    = Actor.create! first_name: 'Aziz', last_name: 'Ansari'
+rashida = Actor.create! first_name: 'Rashida', last_name: 'Jones'
+olan    = Actor.create! first_name: 'Olan', last_name: 'Rogers'
+
+puts 'generating characters'
+Character.create! first_name: 'Dev', show: master_of_none, actor: aziz
+Character.create! first_name: 'Tom', last_name: 'Haverford', show: parks_and_rec, actor: aziz
+Character.create! first_name: 'Ann', last_name: 'Perkins', show: parks_and_rec, actor: rashida
+Character.create! first_name: 'Karen', last_name: 'Filippelli', show: the_office, actor: rashida
+Character.create! first_name: 'Gary', last_name: 'Goodspeed', show: final_space, actor: olan
+
+puts 'done'
